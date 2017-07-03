713afe9be883e6baabd57e865eec6d720b8e15f3
hiroo takaku <slgerkamp@gmail.com>
2017-07-04 02:10:03 +0900

    create rec

---
+++ src/main/java/com/slgerkamp/billy/bass/domain/sound/rec/Record.java
@@ -0,0 +1,108 @@
+package com.slgerkamp.billy.bass.domain.sound.rec;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.AudioFileFormat;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.DataLine;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.Mixer;
+import javax.sound.sampled.TargetDataLine;
+
+public class Record {
+
+	private static final long RECORD_MILLISECOND = 10000;
+
+	// audio file
+	private File wavFile = new File("Record.wav");
+
+	// format of audio file
+	private AudioFileFormat.Type fileType = AudioFileFormat.Type.WAVE;
+
+	// the line from which audio data is captured
+	private TargetDataLine line;
+
+	/**
+	  * Captures the sound and record into a WAV file
+	  */
+	public void start() {
+		Mixer.Info[] infos = AudioSystem.getMixerInfo();
+		for(Mixer.Info info:infos){
+			System.out.println(info.getName());
+			System.out.println(info.getDescription());
+		}
+		try {
+			AudioFormat format = getAudioFormat();
+			DataLine.Info info = new DataLine.Info(TargetDataLine.class, format);
+			// checks if system supports the data line
+			if (!AudioSystem.isLineSupported(info)) {
+				System.out.println("Line not supported");
+				System.exit(0);
+			}
+			line = (TargetDataLine) AudioSystem.getLine(info);
+			line.open(format);
+			line.start();   // start capturing
+
+			System.out.println("Start capturing...");
+
+			AudioInputStream ais = new AudioInputStream(line);
+
+			System.out.println("Start recording...");
+
+			// start recording
+			AudioSystem.write(ais, fileType, wavFile);
+
+		} catch (LineUnavailableException ex) {
+			ex.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
+	/**
+	 * Closes the target data line to finish capturing and recording
+	 */
+	public void finish() {
+		line.stop();
+		line.close();
+		System.out.println("Finished");
+	}
+
+	private AudioFormat getAudioFormat() {
+		float sampleRate = 48000;
+		int sampleSizeInBits = 8;
+		int channels = 2;
+		boolean signed = true;
+		boolean bigEndian = true;
+		AudioFormat format = new AudioFormat(sampleRate, sampleSizeInBits,channels, signed, bigEndian);
+		return format;
+	}
+  
+  /**
+   * Entry to run the program
+   */
+  public static void main(String[] args) {
+    final Record recorder = new Record();
+
+    // creates a new thread that waits for a specified
+// of time before stopping
+Thread stopper = new Thread(new Runnable() {
+  public void run() {
+    try {
+      Thread.sleep(RECORD_MILLISECOND);
+    } catch (InterruptedException ex) {
+      ex.printStackTrace();
+    }
+    recorder.finish();
+  }
+});
+
+stopper.start();
+
+// start recording
+recorder.start();
+}
+}
